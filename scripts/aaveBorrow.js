21deb58a2f5b5b243621f44d130c7b96aeca670a
fbrcode <fabio.bressler@gmail.com>
2022-07-22 23:18:55 -0300

    boiler plate for aave borrow functionality

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,10 @@
+async function main() {
+  // the protocol treats everything asn and ERC20 token
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
